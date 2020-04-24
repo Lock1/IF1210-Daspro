7113df7471ad8f7ca69d8824868f8b4446d3d41e
Lock1 <tanurrizaldi@gmail.com>
2020-04-24 22:13:58 +0700

    Fix sort pada readkritiksaran
    Fix insertion sort pada modul readkritik saran

--- package/base.py
+++ package/base.py
@@ -77,6 +77,8 @@ def appendDatabase(database,insertArray,N):
 def isExistOnDatabase(database,index,search,N,flagToBeToggled=False,indexReturn=False,checkFunction=(lambda a,b: a==b),replace="Null"):
     for i in range(N):
         if (database[i][0] == "~~~"):
+            if (search == "~~~") and indexReturn:
+                return (True,i)
             break
         if (checkFunction(database[i][index],search)):
             flagToBeToggled = not flagToBeToggled
--- package/readkritiksaran.py
+++ package/readkritiksaran.py
@@ -44,15 +44,18 @@ from package.base import *
 
 def sortKritikSaran(kritiksaran,N):
     for i in range(N):
-        for j in range(i-1,-1,j,-1):
-            if ord(kritiksaran[j][2][0])>ord(kritiksaran[j][2][0]):
-                kritiksaran[j],kritiksaran[j+1]=kritiksaran[j+1],kritiksaran[j]
+        for j in range(i-1,-1,-1):
+            if (ord(kritiksaran[j][2][0]) > ord(kritiksaran[j+1][2][0])) and (j >= 0):
+                kritiksaran[j], kritiksaran[j+1] = kritiksaran[j+1], kritiksaran[j]
             else:
                 break
     return kritiksaran
 
 def printKritikSaran(kritiksaran,N=Nmax):
-    sortKritikSaran(kritiksaran,N)
+    (placeholder,markIndex) = isExistOnDatabase(kritiksaran,0,"~~~",N,False,True)
+    kritiksaran = sortKritikSaran(kritiksaran,markIndex)
     print("Kritik dan saran:")
-    for i in range(N):
-        print("{:6}|{:10}|{:20}|{50}".format(kritiksaran[i][2],kritiksaran[i][1],kritiksaran[i][0],kritiksaran[i][3]))
+    for i in range(markIndex):
+        print("{:6} | {:10} | {:30} | {:50}".format(kritiksaran[i][2],kritiksaran[i][1],kritiksaran[i][0],kritiksaran[i][3]))
+
+    print()
